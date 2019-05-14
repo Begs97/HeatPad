51a2ef40af04795b7a3dbb7723aef4aface3582c
Begs97 <joseph.begley23@gmail.com>
2019-05-14 16:15:33 -0400

    Update test.py

--- test.py
+++ test.py
@@ -16,16 +16,13 @@ chan = AnalogIn(ads, ADS.P0)
 # Create differential input between channel 0 and 1
 #chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
-print("{:>5}\t{:>5}".format('raw', 'v'))
-
 while True:
 
     def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
-     import math
-     steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
-     steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
-     steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
-     return steinhart
+        import math
+        steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
+        steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
+        steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
+        return steinhart
 
-print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
 time.sleep(0.5)
