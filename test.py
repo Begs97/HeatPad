6f0afd892e477eb36fc758df96a4bfba3d740fca
Begs97 <joseph.begley23@gmail.com>
2019-05-14 16:26:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,20 @@
 import time
+import math
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
-
-# Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
 
-# Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
-
-# Create differential input between channel 0 and 1
-#chan = AnalogIn(ads, ADS.P0, ADS.P1)
+thermistor = AnalogIn(ads, ADS.P0)
+R = 10000 / (65535/thermistor.value - 1)
 
 while True:
-
-    def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
-        import math
-        steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
-        steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
+    
+        steinhart = math.log(R / 10000) / 3950      # log(R/Ro) / beta
+        steinhart += 1.0 / (25 + 273.15)         # log(R/Ro) / beta + 1/To
         steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
         return steinhart
 
