34c458c03d37c8f7469a63da79d95e89d516134f
Begs97 <joseph.begley23@gmail.com>
2019-05-14 18:36:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -8,13 +8,16 @@ from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 
+def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
+    import math
+    steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
+    steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
+    steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
+    return steinhart
+
 thermistor = AnalogIn(ads, ADS.P0)
 R = 10000 / (65535/thermistor.value - 1)
 
-while True:
+
     
-        steinhart = math.log(R / 10000) / 3950      # log(R/Ro) / beta
-        steinhart += 1.0 / (25 + 273.15)         # log(R/Ro) / beta + 1/To
-        steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
-        Print ("Steinhart=", steinhart)
-        time.sleep(0.5)
+
