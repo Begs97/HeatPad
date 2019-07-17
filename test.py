53374bfffb2dcdbc1dfe0c87f3739d8148e36c12
Begs97 <joseph.begley23@gmail.com>
2019-07-17 12:33:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import time
 import math
 import board
@@ -6,6 +7,14 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import RPi.GPIO as GPIO
 GPIO.setup(12, GPIO.OUT)
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 10), ylim=(0, 70))
+line, = ax.plot([], [], lw=3)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
@@ -17,13 +26,30 @@ p.start(50)
 # p.ChangeDutyCycle(dc)
 # p.ChangeFrequency(freq)
 
+def init():
+    line.set_data([], [])
+    return line
+
 def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
-    import math
     steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
     steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
     steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
     return steinhart
 
+def animate(i):
+    x = np.linspace(0, 4, 1000)
+    y = S
+    line.set_data(x, y)
+    return line
+    
+    plt.xlabel('Time (S)')
+    plt.ylabel('Temperature (C)')
+    plt.title('Heating Pad')
+    
+ani = animation.FuncAnimation(fig, animate, init_func=init,
+                              frames=200, interval=1000, blit=True)
+
+plt.show()
 
 while True:
     R = ((26407 / thermistor.value) - 1) * 10000
@@ -31,5 +57,4 @@ while True:
     print('ADC =', thermistor.value, 'Temp = ', format(S, '.2f'), 'Resistance = ', format(R, '.2f'))
     time.sleep(.5)
   
-
     
