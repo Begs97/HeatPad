05eae0509606ba64f4789877237e8398dbdf8e12
Begs97 <joseph.begley23@gmail.com>
2020-01-08 11:46:08 -0500

    Update 1-8-20tk.py

--- 1-8-20tk.py
+++ 1-8-20tk.py
@@ -67,7 +67,7 @@ def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
     steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
     return steinhart
 
-def get_PV()
+def get_PV():
     # Read temperature (Celsius) from the steinhart equation
     R = ((26407 / chan0.value) - 1) * 10000
     PV = steinhart_temperature_C(R)
@@ -186,6 +186,25 @@ class HeatPadapp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+        button = ttk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = ttk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Graph Page",
+                            command=lambda: controller.show_frame(PageThree))
+        button3.pack()
+        
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
