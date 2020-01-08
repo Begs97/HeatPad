621476554576c72f78b50a1d478b2e482c6e035f
Begs97 <joseph.begley23@gmail.com>
2020-01-08 15:52:46 -0500

    Update 1-8-20tk.py

--- 1-8-20tk.py
+++ 1-8-20tk.py
@@ -23,6 +23,7 @@ import RPi.GPIO as GPIO
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 from scipy.interpolate import make_interp_spline
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 import threading
@@ -156,106 +157,22 @@ time_list = []
 
 #####################  Main Loop  ############################
 
-class HeatPadapp(tk.Tk):
+class Example(Frame):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+
+        self.parent = parent        
         
-        tk.Tk.__init__(self, *args, **kwargs)
-
-#       tk.Tk.iconbitmap(self, default="clienticon.ico")
-        tk.Tk.wm_title(self, "Heat Pad Controller")
-        HeatPadapp.geometry("320x240")
         
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+class main():
+    def __init__(self):
+        root = Tk()
+        ex = Example(root)
+        root.minsize(320, 240)
+        root.mainloop()  
+    
 
-        self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo, PageThree):
-
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-
-        frame = self.frames[cont]
-        frame.tkraise()
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-        
-        button = ttk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
-        button.pack()
-
-        button2 = ttk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-        button3 = ttk.Button(self, text="Graph Page",
-                            command=lambda: controller.show_frame(PageThree))
-        button3.pack()
-        
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Temperature", font=LARGE_FONT)
-        label.grid(pady=10,padx=10,row=0,column=1)
-        button1 = ttk.Button(self, text="Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.grid(pady=2,padx=5,row=6,column=1)
-
-        button2 = ttk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.grid(pady=2,padx=5,row=7,column=1)
-        
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-
-
-class PageThree(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()        
-
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-app = HeatPadapp()
-ani = animation.FuncAnimation(f, animate, interval=1000)
-app.mainloop()
 
 ###############################################################
 
