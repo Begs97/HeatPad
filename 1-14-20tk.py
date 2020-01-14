b34043e79ad9dc463b92c56cceb55614bcd58816
Begs97 <joseph.begley23@gmail.com>
2020-01-14 15:29:02 -0500

    Update 1-14-20tk.py

--- 1-14-20tk.py
+++ 1-14-20tk.py
@@ -245,7 +245,7 @@ class PageThree(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label = tk.Label(self, text="Target: %.1f C | Current: %.1f C | PWM: %s %%"%(SP, PV, OP), font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
         button1 = ttk.Button(self, text="Back to Home",
