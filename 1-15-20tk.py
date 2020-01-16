4969c24ac951c660386ccda766b0fa5afc509fc8
Begs97 <joseph.begley23@gmail.com>
2020-01-16 15:42:45 -0500

    Update 1-15-20tk.py

--- 1-15-20tk.py
+++ 1-15-20tk.py
@@ -67,63 +67,70 @@ def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
 
 #################  Push Button Callbacks  #####################
 
-Class increment():
-    def __init__(self, *args, **kwargs):
-        global SP
+increment_mode = 1
+IM = "^"
+        
+def cb17(channel):
+    print("GPIO 17 Detected")
+        
+def increase_sp_callback(channel):
+    global SP
+    global increment_mode
+    if SP >= 69:
+        print("Set Point is at max value")
+        print("SP=", SP)
+    elif increment_mode == 1:
+        SP += 1
+        print("increase detected")
+        print("SP=", SP)
+    elif increment_mode == 2:
+        SP += .1
+        print("increase detected")
+        print("SP=", SP)
+    elif increment_mode == 3:
+        SP += .01
+        print("increase detected")
+        print("SP=", SP)
+
+def decrease_sp_callback(channel):
+    global SP
+    global increment_mode
+    if SP <= 20:
+        print("Set Point is at minimum value")
+        print("SP=", SP)
+    elif increment_mode == 1:
+        SP -= 1
+        print("decrease detected")
+        print("SP=", SP)
+    elif increment_mode == 2:
+        SP -= .1
+        print("decrease detected")
+        print("SP=", SP)
+    elif increment_mode == 3:
+        SP -= .01
+        print("decrease detected")
+        print("SP=", SP)
+
+def cb27(channel):
+    global increment_mode
+    global IM
+    if increment_mode == 1:
+        increment_mode = 2
+        IM = " ^"
+        print("Increment Mode Changed to 0.10")
+    elif increment_mode == 2:
+        increment_mode = 3
+        IM = "  ^"
+        print("Increment Mode Changed to 0.01")
+    elif increment_mode == 3:
         increment_mode = 1
-        
-        def cb17(channel):
-            print("GPIO 17 Detected")
-        
-        def increase_sp_callback(channel):         
-            if SP >= 69:
-                print("Set Point is at max value")
-                print("SP=", SP)
-            elif increment_mode = 1:
-                SP += 1
-                print("increase detected")
-                print("SP=", SP)
-            elif increment_mode = 2:
-                SP += .1
-                print("increase detected")
-                print("SP=", SP)
-            elif increment_mode = 3:
-                SP += .01
-                print("increase detected")
-                print("SP=", SP)
+        IM = "^"
+        print("Increment Mode Changed to 1.00")
 
-        def decrease_sp_callback(channel):
-            if SP <= 20:
-                print("Set Point is at minimum value")
-                print("SP=", SP)
-            elif increment_mode = 1:
-                SP -= 1
-                print("decrease detected")
-                print("SP=", SP)
-            elif increment_mode = 2:
-                SP -= .1
-                print("decrease detected")
-                print("SP=", SP)
-            elif increment_mode = 3:
-                SP -= .01
-                print("decrease detected")
-                print("SP=", SP)
-
-        def cb27(channel):
-            if increment_mode = 1:
-                increment_mode = 2
-                print("Increment Mode Changed to 0.10")
-            elif increment_mode = 2:
-                increment_mode = 3
-                print("Increment Mode Changed to 0.01")
-            elif increment_mode = 3:
-                increment_mode = 1
-                print("Increment Mode Changed to 1.00")
-
-        GPIO.add_event_detect(17, GPIO.FALLING, callback=cb17)
-        GPIO.add_event_detect(22, GPIO.FALLING, callback=increase_sp_callback)
-        GPIO.add_event_detect(23, GPIO.FALLING, callback=decrease_sp_callback)
-        GPIO.add_event_detect(27, GPIO.FALLING, callback=cb27)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=cb17)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=increase_sp_callback)
+GPIO.add_event_detect(23, GPIO.FALLING, callback=decrease_sp_callback)
+GPIO.add_event_detect(27, GPIO.FALLING, callback=cb27)
 
 ################  Create Figure for Plotting  #################
 
@@ -174,6 +181,7 @@ def animate(i, xs, ys, y2):
     ax.clear()
     ax.plot(xs, ys, '-b', label='PV = %.2f\u2103' %PV)
     ax.plot(xs, y2, 'r--', label='SP = %.2f\u2103' %SP)
+    ax.plot([],[],' ', label='      %s' %IM)
     ax.legend(framealpha=1, frameon=True, loc = 'upper left', fontsize=12)
     
     # set the limit on the y-axis
